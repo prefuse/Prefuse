fad795909035f0d980258278cd32e88d1e2ab654
Sasti <sasti9@online.de>
2013-10-24 12:54:12 +0200

    Date and TimeParser changed so that the beahavior is more reliable. The format is now directly defined through SimpleDateFormater instances in the constructors of those classes.
    DateTimeParser changed so that it is compatible to the new DateParser constructor.

--- src/prefuse/data/parser/TimeParser.java
+++ src/prefuse/data/parser/TimeParser.java
@@ -2,15 +2,15 @@ package prefuse.data.parser;
 
 import java.sql.Time;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * DataParser instance that parses Date values as java.util.Time instances,
- * representing a particular time (but no specific date).
- * This class uses a backing {@link java.text.DateFormat} instance to
- * perform parsing. The DateFormat instance to use can be passed in to the
- * constructor, or by default the DateFormat returned by
- * {@link java.text.DateFormat#getTimeInstance(int)} with an
- * argument of {@link java.text.DateFormat#SHORT} is used.
+ * representing a particular time (but no specific date). This class uses a
+ * backing {@link java.text.DateFormat} instance to perform parsing. The
+ * DateFormat instance to use can be passed in to the constructor, or by default
+ * the DateFormat returned by {@link java.text.DateFormat#getTimeInstance(int)}
+ * with an argument of {@link java.text.DateFormat#SHORT} is used.
  *
  * @author <a href="http://jheer.org">jeffrey heer</a>
  */
@@ -20,25 +20,30 @@ public class TimeParser extends DateParser {
      * Create a new TimeParser.
      */
     public TimeParser() {
-        this(DateFormat.getTimeInstance(DateFormat.SHORT));
+        this(new DateFormat[]{
+            new SimpleDateFormat("KK:mm a"),
+            new SimpleDateFormat("HH:mm")
+        });
     }
-    
+
     /**
      * Create a new TimeParser.
+     *
      * @param dateFormat the DateFormat instance to use for parsing
      */
-    public TimeParser(DateFormat dateFormat) {
-        super(dateFormat);
+    public TimeParser(DateFormat[] dateFormats) {
+        super(dateFormats);
     }
-    
+
     /**
      * Returns java.sql.Time.class.
+     *
      * @see prefuse.data.parser.DataParser#getType()
      */
     public Class getType() {
         return Time.class;
     }
-    
+
     /**
      * @see prefuse.data.parser.DataParser#canParse(java.lang.String)
      */
@@ -46,20 +51,21 @@ public class TimeParser extends DateParser {
         try {
             parseTime(val);
             return true;
-        } catch ( DataParseException e ) {
+        } catch (DataParseException e) {
             return false;
         }
     }
-    
+
     /**
      * @see prefuse.data.parser.DataParser#parse(java.lang.String)
      */
     public Object parse(String val) throws DataParseException {
         return parseTime(val);
     }
-    
+
     /**
      * Parse a Time value from a text string.
+     *
      * @param text the text string to parse
      * @return the parsed Time value
      * @throws DataParseException if an error occurs during parsing
@@ -67,29 +73,33 @@ public class TimeParser extends DateParser {
     public Time parseTime(String text) throws DataParseException {
         m_pos.setErrorIndex(0);
         m_pos.setIndex(0);
-        
+
         // parse the data value, convert to the wrapper type
         Time t = null;
         try {
             t = Time.valueOf(text);
             m_pos.setIndex(text.length());
-        } catch ( IllegalArgumentException e ) {
+        } catch (IllegalArgumentException e) {
             t = null;
         }
-        if ( t == null ) {
-            java.util.Date d1 = m_dfmt.parse(text, m_pos);
-            if ( d1 != null ) {
-                t = new Time(d1.getTime());
+        if (t == null) {
+            // interate through all known time formats and try to parse
+            for (int i = 0; i < m_dfmt.length; i++) {
+                DateFormat dateFormat = m_dfmt[i];
+                java.util.Date d1 = dateFormat.parse(text, m_pos);
+                if (d1 != null) {
+                    t = new Time(d1.getTime());
+                    break;
+                }
             }
         }
-        
+
         // date format will parse substrings successfully, so we need
         // to check the position to make sure the whole value was used
-        if ( t == null || m_pos.getIndex() < text.length() ) {
-            throw new DataParseException("Could not parse Date: "+text);
+        if (t == null || m_pos.getIndex() < text.length()) {
+            throw new DataParseException("Could not parse Date: " + text);
         } else {
             return t;
         }
     }
-        
 } // end of class TimeParser
